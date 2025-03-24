8455fe2a70ee0b0e2209189042b73f228c91f86c
Methois <87484579+Methois@users.noreply.github.com>
2025-03-24 23:32:32 +0100

    Update mijn_spel.py

--- Opdracht 14/mijn_spel.py
+++ Opdracht 14/mijn_spel.py
@@ -1,17 +1,24 @@
-import pygame
+import pgzrun
 from pgzero.actor import Actor
 
 width = 800
 height = 600
-screen = pygame.display.set_mode((width, height))
-speler = Actor("alien.png", (400, 300))
+
+# Zorg ervoor dat de afbeelding in de 'images' map staat
+speler = Actor("apple.png", (400, 300))  # Dit verwijst naar 'images/apple.png'
 
 def draw():
     screen.clear()
     speler.draw()
 
 def update():
-    if keyboard.left:
+    if keyboard.left and speler.x > 0:
         speler.x -= 5
-    if keyboard.right:
+    if keyboard.right and speler.x < width:
         speler.x += 5
+    if keyboard.up and speler.y > 0:
+        speler.y -= 5
+    if keyboard.down and speler.y < height:
+        speler.y += 5
+
+pgzrun.go()
